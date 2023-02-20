df1106ee6b2b5d240ffffb0b2c5685aabc74a1b7
Nicolas Oliveira <nicolasm15@hotmail.com>
2023-02-20 15:24:12 -0300

    feat: adicionando código do simulador em GoLang

---
+++ Makefile
@@ -0,0 +1,4 @@
+up-simulator:
+	cd simulator && docker compose up -d
+sh-simulator:
+	cd simulator && docker exec -it simulator bash
---
+++ simulator/application/route.go
@@ -0,0 +1,87 @@
+package route
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Route struct {
+	ID        string
+	ClientID  string
+	Positions []Position
+}
+
+type Position struct {
+	Lat  float64
+	Long float64
+}
+
+type PartialRoutePosition struct {
+	ID       string    `json:"routeId"`
+	ClientID string    `json:"clientId"`
+	Position []float64 `json:"position"`
+	Finished bool      `json:"finished"`
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
+	file, err := os.Open("/destinations/" + r.ID + ".txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+
+		lat, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return err
+		}
+		
+		long, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return err
+		}
+		
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+	return nil
+}
+
+func (r *Route) ExportJsonPositions() ([]string, error) {
+	var route PartialRoutePosition
+	var result []string
+
+	total := len(r.Positions)
+	
+	for k, v := range r.Positions {
+		route.ID = r.ID
+		route.ClientID = r.ClientID
+		route.Position = []float64{v.Lat, v.Long}
+		route.Finished = false
+		
+		if total-1 == k {
+			route.Finished = true
+		}
+		
+		jsonRoute, err := json.Marshal(route)
+		if err != nil {
+			return nil, err
+		}
+		
+		result = append(result, string(jsonRoute))
+	}
+	return result, nil
+}
